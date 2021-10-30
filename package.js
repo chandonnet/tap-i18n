25ed1375fe8b470552ff7c07a8d8698d758f32dc
Francis Chandonnet <franki_football@hotmail.com>
2021-10-29 20:39:34 -0400

    Fix use of deprecated API in package.js

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-	name: 'wolas:tap-i18n',
+	name: 'fchandonnet:i18n',
 	summary: 'A comprehensive internationalization solution for Meteor',
 	version: '1.9.0',
-	git: 'https://github.com/wolasss/tap-i18n',
+	git: 'https://github.com/chandonnet/tap-i18n',
 });
 
 const both = ['server', 'client'];
@@ -10,7 +10,7 @@ const server = 'server';
 const client = 'client';
 
 Package.onUse(function(api) {
-	api.versionsFrom('2.2');
+	api.versionsFrom('2.3');
 
 	api.use('coffeescript@1.0.17', both);
 	api.use('underscore', both);
@@ -27,25 +27,25 @@ Package.onUse(function(api) {
 	api.use('cfs:http-methods@0.0.27', server);
 
 	// load TAPi18n
-	api.add_files('lib/globals.js', both);
+	api.addFiles('lib/globals.js', both);
 
 	// load and init TAPi18next
-	api.add_files('lib/tap_i18next/tap_i18next-1.7.3.js', both);
+	api.addFiles('lib/tap_i18next/tap_i18next-1.7.3.js', both);
 	api.export('TAPi18next');
-	api.add_files('lib/tap_i18next/tap_i18next_init.js', both);
+	api.addFiles('lib/tap_i18next/tap_i18next_init.js', both);
 
-	api.add_files('lib/tap_i18n/tap_i18n-helpers.coffee', both);
+	api.addFiles('lib/tap_i18n/tap_i18n-helpers.coffee', both);
 
 	// We use the bare option since we need TAPi18n in the package level and
 	// coffee adds vars to all (so without bare all vars are in the file level)
-	api.add_files('lib/tap_i18n/tap_i18n-common.coffee', server);
-	api.add_files('lib/tap_i18n/tap_i18n-common.coffee', client, { bare: true });
+	api.addFiles('lib/tap_i18n/tap_i18n-common.coffee', server);
+	api.addFiles('lib/tap_i18n/tap_i18n-common.coffee', client, { bare: true });
 
-	api.add_files('lib/tap_i18n/tap_i18n-server.coffee', server);
-	api.add_files('lib/tap_i18n/tap_i18n-client.coffee', client, { bare: true });
+	api.addFiles('lib/tap_i18n/tap_i18n-server.coffee', server);
+	api.addFiles('lib/tap_i18n/tap_i18n-client.coffee', client, { bare: true });
 
-	api.add_files('lib/tap_i18n/tap_i18n-init.coffee', server);
-	api.add_files('lib/tap_i18n/tap_i18n-init.coffee', client, { bare: true });
+	api.addFiles('lib/tap_i18n/tap_i18n-init.coffee', server);
+	api.addFiles('lib/tap_i18n/tap_i18n-init.coffee', client, { bare: true });
 
 	api.export('TAPi18n');
 });
